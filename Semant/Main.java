da7b88820a10f4dd5a250648eab7994d36ec3fa8
Ryan T Naugle <rnaugle77@gmail.com>
2015-02-23 19:41:15 -0600

    visitor

--- Semant/Main.java
+++ Semant/Main.java
@@ -78,30 +78,14 @@ public class Main {
     	classNames.add(cd.name);
     	//CLASS c1 = new CLASS(Symbol.symbol(cd.name));
     	
-    	RECORD methods = new RECORD();
-    	for(Semant.Absyn.MethodDecl md : cd.methods){
-    		RECORD formals = new RECORD;
-    		for(Semant.Absyn.Formal : md.params){
-    			//formals.put();
-    		}
-    		methods.put(new FUNCTION(Symbol.symbol(md.name),null, new RECORD(), null), Symbol.symbol(md.name));
-    		
-    	}
-  
-    	RECORD fields = new RECORD();
-    	for(Semant.Absyn.VarDecl vd : cd.fields){
-    		fields.put(null, Symbol.symbol(vd.name));
-    	}
-    	c1.methods = methods;
-    	
-    	c1.fields = fields;
+    
     	
    
     	c1.methods = null;
     	
-    	//c1.fields = null;
-    	//c1.parent = null;
-    	//c1.instance = null;
+    	c1.fields = null;
+    	c1.parent = null;
+    	c1.instance = null;
     	//tb.put(c1.name, c1);
     }
     
@@ -125,6 +109,24 @@ public class Main {
         	    		
         	}    	
         	//OBJECT instance = new OBJECT(c1, );
+        	RECORD methods = new RECORD();
+        	for(Semant.Absyn.MethodDecl md : cd.methods){
+        		RECORD formals = new RECORD;
+        		for(Semant.Absyn.Formal form: md.params){
+        			if(form.type instanceof )
+        			formals.put(Symbol.symbol(formal), );
+        		}
+        		methods.put(new FUNCTION(Symbol.symbol(md.name),null, new RECORD(), null), Symbol.symbol(md.name));
+        		
+        	}
+      
+        	RECORD fields = new RECORD();
+        	for(Semant.Absyn.VarDecl vd : cd.fields){
+        		fields.put(null, Symbol.symbol(vd.name));
+        	}
+        	c1.methods = methods;
+        	
+        	c1.fields = fields;
     	}
     	
     	
