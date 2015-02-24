d32e5353d99cac05fd61b29cd54c2fc24d142373
nh137 <nidahafeez137@gmail.com>
2015-02-23 22:03:17 -0600

    added to classvisitor, may need to get rid of main stuff

--- Semant/Visit/ClassVisitor.java
+++ Semant/Visit/ClassVisitor.java
@@ -13,22 +13,105 @@ public ClassVisitor extends Visitor{
 	}
 	
 	
-	public Semant.Types.Type visit(Absyn a);
-	  public Semant.Types.Type visit(AddExpr e);
-	  public Semant.Types.Type visit(AndExpr e);
-	  public Semant.Types.Type visit(ArrayExpr e);
+		public Semant.Types.Type visit(Absyn a);
+		
+	  public Semant.Types.Type visit(AddExpr e)
+	  {	
+		Semant.Types.Type t1 = e.e1.accept(this);
+		Semant.Types.Type t2 = e.e2.accept(this);
+		if(t1 instanceof Semant.Types.INT && t2 instanceof Semant.Types.INT)
+			t1.accept(this);
+		else 
+		{
+			System.err.println("ERROR operator + cannot be applied to " + t1.toString() + ", " + t2.toString()
+			+ "\n" + Semant.Visit.AbsynPrintVisitor.visit(e) );
+		}
+	  }
+	  
+	  public Semant.Types.Type visit(AndExpr e)
+	  {
+		Semant.Types.Type t1 = e.e1.accept(this);
+		Semant.Types.Type t2 = e.e2.accept(this);
+		if(t1 instanceof Semant.Types.BOOLEAN && t2 instanceof Semant.Types.BOOLEAN)
+			t1.accept(this);
+		else 
+		{
+			System.err.println("ERROR operator && cannot be applied to " + t1.toString() + ", " + t2.toString()
+			+ "\n" + Semant.Visit.AbsynPrintVisitor.visit(e) );
+		}
+	  }
+	  
+	  public Semant.Types.Type visit(ArrayExpr e)
+	  {	
+		Semant.Types.Type t1 = e.e1.accept(this);
+		Semant.Types.Type t2 = e.e2.accept(this);
+		if(t1 instanceof Semant.Types.ARRAY && t2 instanceof Semant.Types.INT)
+			t1.accept(this);
+		else if(!t1 instanceof Semant.Types.ARRAY)
+		{
+			System.err.println("ERROR incompatible types: array required, but " + t1.toString() + " found, "
+			+ "\n" + Semant.Visit.AbsynPrintVisitor.visit(e.e2) );
+		}
+		else	
+		{
+			System.err.println("ERROR incompatible types: int required, but " + t1.toString() + " found, "
+			+ "\n" + Semant.Visit.AbsynPrintVisitor.visit(e) );
+		}
+	  }
+	  
 	  public Semant.Types.Type visit(ArrayType e){
 		  Semant.Types.Type t = e.base;
 		  return new Semant.Types.ARRAY(t);
 	  }
-	  public Semant.Types.Type visit(AssignableExpr e);
-	  public Semant.Types.Type visit(AssignStmt e);
-	  public Semant.Types.Type visit(BinOpExpr e);
-	  public Semant.Types.Type visit(BlockStmt e);
+	  
+	  public Semant.Types.Type visit(AssignableExpr e)
+		{ 
+			return e.accept(this);
+		}
+	  
+	  public Semant.Types.Type visit(AssignStmt e)
+	  {
+		Semant.Types.Type t1 = e.e1.accept(this);
+		Semant.Types.Type t2 = e.e2.accept(this);
+		if(!t2.coerceTo(t1))
+			System.err.println("ERROR incompatible types: "+ t1.toString() +" required, but " + t2.toString() +" found");
+		else 
+			return t1;
+	  }
+	  
+	  public Semant.Types.Type visit(BinOpExpr e)
+	  {
+		Semant.Types.Type t1 = e.e1.accept(this);
+		Semant.Types.Type t2 = e.e2.accept(this);
+		if (t1 instanceof BOOLEAN && t2 instanceof BOOLEAN)
+			return t1;
+		else
+			System.err.println("ERROR operator cannot be applied to " + t1.toString() +", "+t2.toString());
+	  }
+	  
+	  public Semant.Types.Type visit(BlockStmt e){
+		for (Stmt s : e.stmts)
+		{
+			s.accept(this);
+		}
+	}
+		
 	  public Semant.Types.Type visit(BooleanType e){
 		  return new Semant.Types.BOOLEAN();
 	  }
-	  public Semant.Types.Type visit(CallExpr e);
+	  
+	  public Semant.Types.Type visit(CallExpr e){
+		Semant.Types.Type t = e.target.accept(this);
+		Semant.Types.Type tm = ct.get(Symbol.symbol(e.method));
+		
+		//TODO count formals
+		
+		if (tm == null || !tm instanceof FUNCTION)
+			System.err.println("ERROR cannot resolve method " + e.method);
+		else
+			
+			
+	  }
 	  public Semant.Types.Type visit(ClassDecl e){
 	
 		  ot.beginScope();
@@ -113,7 +196,7 @@ public ClassVisitor extends Visitor{
 		  if(t1.coerceTo(t)){
 			  ct.put(Symbol.symbol(e.name), t);
 		  }else{
-			  if(t instance of Semant.Types.ARRAY){
+			  if(t instanceof Semant.Types.ARRAY){
 				  System.err.println("ERROR incompatible type: array required, but " + t1.toString()+" found");
 			  }else{
 				  System.err.println("ERROR incompatible type: " + t.toString()+" required, but " + t1.toString()+" found");
